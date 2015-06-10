0a456ff276f8278816b4b124394aa7402e042ca9
Dan Thareja <danthareja@gmail.com>
2015-06-10 15:37:29 -0700

    fix(typo): comonent => component

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -1,11 +1,11 @@
 import template from './app.html!text';
 import './app.css!';
 
-let appComonent = ()=>{
+let appComponent = ()=>{
 	return {
 		template, // because we have a variable name template we can use the shorcut here
 		restrict: 'E'
 	};
 };
 
-export default appComonent;
+export default appComponent;
