97df11a5904d44e766b49f286077217f059506d7
Jordan Belford <jordan.g.belford@gmail.com>
2015-07-24 14:00:08 -0700

    add missing !s to component template

--- generator/component/temp.component.js
+++ generator/component/temp.component.js
@@ -1,6 +1,6 @@
 import template from './<%= name %>.html!text';
 import controller from './<%= name %>.controller';
-import './<%= name %>.css';
+import './<%= name %>.css!';
 
 let <%= name %>Component = function(){
 	return {
