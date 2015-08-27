e0dd6df98e1d0efdb22f77300da88b2ce65fe10b
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:34:06 -0700

    Update temp.spec.js

--- generator/component/temp.spec.js
+++ generator/component/temp.spec.js
@@ -3,65 +3,51 @@ import <%= upCaseName %>Controller from './<%= name %>.controller';
 import <%= upCaseName %>Component from './<%= name %>.component';
 import <%= upCaseName %>Template from './<%= name %>.html';
 
-describe('<%= upCaseName %>', ()=>{
-	let $rootScope,
-	makeController;
-	
-	beforeEach(window.module(<%= upCaseName %>Module.name));
-	beforeEach(inject((_$rootScope_)=>{
-		$rootScope = _$rootScope_;
-		makeController = ()=>{
-			return new <%= upCaseName %>Controller();
-		};
-	}));
-	
-	describe('Module', ()=>{
-		// test things about the component module
-		// checking to see if it registers certain things and what not
-		// test for best practices with naming too
-		// test for routing
-	});
-	
-	describe('Controller', ()=>{
-		// test your controller here
-		
-		it('should have a name property [REMOVE]', ()=>{ // erase me if you remove this.name from the controller
-			let controller = makeController();
-			
-			expect(controller).to.have.property('name'); 
-		});
-	});
-	
-	describe('Template', ()=>{
-		// test the template
-		// use Regexes to test that you are using the right bindings {{  }}
-		
-		it('should have name in template [REMOVE]', ()=>{
-			expect(<%= upCaseName %>Template).to.match(/{{\s?vm\.name\s?}}/g);
-		});
-	});
-	
-	
-	describe('Component', ()=>{
-			// test the component/directive itself
-			let component = <%= upCaseName %>Component();
-			
-			it('should use the right template',()=>{
-				expect(component.template).to.equal(<%= upCaseName %>Template);
-			});
-			
-			it('should use controllerAs', ()=>{
-				expect(component).to.have.property('controllerAs');
-			});
-			
-			it('should use the right controller', ()=>{
-				expect(component.controller).to.equal(<%= upCaseName %>Controller);
-			});
-	});
+describe('<%= upCaseName %>', () => {
+  let $rootScope, makeController;
+
+  beforeEach(window.module(<%= upCaseName %>Module.name));
+  beforeEach(inject((_$rootScope_) => {
+    $rootScope = _$rootScope_;
+    makeController = () => {
+      return new <%= upCaseName %>Controller();
+    };
+  }));
+
+  describe('Module', () => {
+    // top-level specs: i.e., routes, injection, naming
+  });
+
+  describe('Controller', () => {
+    // controller specs
+    it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
+      let controller = makeController();
+      expect(controller).to.have.property('name');
+    });
+  });
+
+  describe('Template', () => {
+    // template specs
+    // tip: use regex to ensure correct bindings are used e.g., {{  }}
+    it('has name in template [REMOVE]', () => {
+      expect(<%= upCaseName %>Template).to.match(/{{\s?vm\.name\s?}}/g);
+    });
+  });
+
+  describe('Component', () => {
+      // component/directive specs
+      let component = <%= upCaseName %>Component();
+
+      it('includes the intended template',() => {
+        expect(component.template).to.equal(<%= upCaseName %>Template);
+      });
+
+      it('uses `controllerAs` syntax', () => {
+        expect(component).to.have.property('controllerAs');
+      });
+
+      it('invokes the right controller', () => {
+        expect(component.controller).to.equal(<%= upCaseName %>Controller);
+      });
+  });
 });
-
- 
-
-
-
-
