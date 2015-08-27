7db6d6447cd658d3ec70ea4a2f0d92cc4fc4b19a
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:37:24 -0700

    Update temp.component.js

--- generator/component/temp.component.js
+++ generator/component/temp.component.js
@@ -2,15 +2,15 @@ import template from './<%= name %>.html';
 import controller from './<%= name %>.controller';
 import './<%= name %>.styl';
 
-let <%= name %>Component = function(){
-	return {
-		template,
-		controller,
-		restrict: 'E',
-		controllerAs: 'vm',
-		scope: {},
-		bindToController: true
-	};
+let <%= name %>Component = function () {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
 };
 
 export default <%= name %>Component;
