202a9042329a315bdb27d57e61d0eea78f390f61
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:13:57 -0700

    Update home.component.js

--- client/app/components/home/home.component.js
+++ client/app/components/home/home.component.js
@@ -2,15 +2,15 @@ import template from './home.html';
 import controller from './home.controller';
 import './home.styl';
 
-let homeComponent = function(){
-	return {
-		template,
-		controller,
-		restrict: 'E',
-		controllerAs: 'vm',
-		scope: {},
-		bindToController: true
-	};
+let homeComponent = function () {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
 };
 
 export default homeComponent;
