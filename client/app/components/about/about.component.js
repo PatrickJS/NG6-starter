1c821585b8e0fad382f57339ea29fe9611407f82
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:17:54 -0700

    Update about.component.js

--- client/app/components/about/about.component.js
+++ client/app/components/about/about.component.js
@@ -2,15 +2,15 @@ import template from './about.html';
 import controller from './about.controller';
 import './about.styl';
 
-let aboutComponent = function(){
-	return {
-		template,
-		controller,
-		restrict: 'E',
-		controllerAs: 'vm',
-		scope: {},
-		bindToController: true
-	};
+let aboutComponent = function () {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
 };
 
 export default aboutComponent;
