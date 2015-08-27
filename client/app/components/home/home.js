6174e13bea52d2869a19a48bc2574727b4f75381
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:12:09 -0700

    Update home.js

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,19 +1,21 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import homeComponent from './home.component'; 
+import homeComponent from './home.component';
 
 let homeModule = angular.module('home', [
-	uiRouter
+  uiRouter
 ])
-.config(($stateProvider, $urlRouterProvider)=>{
-	$urlRouterProvider.otherwise('/');
-	
-	$stateProvider
-		.state('home', {
-			url: '/',
-			template: '<home></home>'
-		});
+
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<home></home>'
+    });
 })
+
 .directive('home', homeComponent);
 
 export default homeModule;
