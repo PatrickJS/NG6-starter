eaf7f711fb8d58b0cca069fc4d5f94bfe855e76b
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:45:11 -0700

    Update components.js

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about'; 
+import About from './about/about';
 
 let componentModule = angular.module('app.components', [
-	Home.name,
-	About.name
+  Home.name,
+  About.name
 ]);
 
 export default componentModule;
