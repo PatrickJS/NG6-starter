c14576ce1fb7b272cf648fc1c00d45daddeacf7e
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:03:56 -0700

    Update hero.component.js

--- client/app/common/hero/hero.component.js
+++ client/app/common/hero/hero.component.js
@@ -2,15 +2,15 @@ import template from './hero.html';
 import controller from './hero.controller';
 import './hero.styl';
 
-let heroComponent = function(){
-	return {
-		template,
-		controller,
-		restrict: 'E',
-		controllerAs: 'vm',
-		scope: {},
-		bindToController: true
-	};
+let heroComponent = function () {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
 };
 
 export default heroComponent;
