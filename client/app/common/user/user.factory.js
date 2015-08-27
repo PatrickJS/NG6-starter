d0c9d8ff458e52ad4902f4b4059878fd8a218083
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:50:13 -0700

    Update user.factory.js

--- client/app/common/user/user.factory.js
+++ client/app/common/user/user.factory.js
@@ -1,15 +1,15 @@
-let UserFactory = function(){
-	const user = {};
-	
-	let getUser = ()=>{
-		return user;			
-	};
-	
-	let isSignedIn = ()=>{
-		return user.isSignedIn; 
-	};
+let UserFactory = function () {
+  const user = {};
 
-	return { getUser, isSignedIn };
+  let getUser = () => {
+    return user;
+  };
+
+  let isSignedIn = () => {
+    return user.isSignedIn; 
+  };
+
+  return { getUser, isSignedIn };
 };
 
-export default UserFactory; 
+export default UserFactory;
