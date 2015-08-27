44c5d99e9a8b1f7f9ee5e193b187c1f1fe53e5af
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:41:57 -0700

    Update app.js

--- client/app/app.js
+++ client/app/app.js
@@ -6,8 +6,9 @@ import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
-	uiRouter,
-	Common.name,
-	Components.name
+  uiRouter,
+  Common.name,
+  Components.name
 ])
+
 .directive('app', AppComponent);
