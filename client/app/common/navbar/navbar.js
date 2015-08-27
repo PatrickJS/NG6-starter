b62cce0b2b7c4853c612f97739bc2522c22a84e4
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:00:36 -0700

    Update navbar.js

--- client/app/common/navbar/navbar.js
+++ client/app/common/navbar/navbar.js
@@ -1,10 +1,11 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import navbarComponent from './navbar.component'; 
+import navbarComponent from './navbar.component';
 
 let navbarModule = angular.module('navbar', [
-	uiRouter
+  uiRouter
 ])
+
 .directive('navbar', navbarComponent);
 
 export default navbarModule;
