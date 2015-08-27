66c4bfd5c562f7a6a24b39131a44ecb873bfc7d0
Nima Mehanian <nima@mehanian.com>
2015-08-26 23:35:11 -0700

    Update temp.js

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -1,10 +1,11 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import <%= name %>Component from './<%= name %>.component'; 
+import <%= name %>Component from './<%= name %>.component';
 
 let <%= name %>Module = angular.module('<%= name %>', [
-	uiRouter
+  uiRouter
 ])
+
 .directive('<%= name %>', <%= name %>Component);
 
 export default <%= name %>Module;
