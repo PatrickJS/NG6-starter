a303c554c0e10a140cd22a4eeb2da6037faf63f8
Nima Mehanian <nima@mehanian.com>
2015-08-27 00:02:50 -0700

    Update navbar.component.js

--- client/app/common/navbar/navbar.component.js
+++ client/app/common/navbar/navbar.component.js
@@ -2,15 +2,15 @@ import template from './navbar.html';
 import controller from './navbar.controller';
 import './navbar.styl';
 
-let navbarComponent = function(){
-	return {
-		template,
-		controller,
-		restrict: 'E',
-		controllerAs: 'vm',
-		scope: {},
-		bindToController: true
-	};
+let navbarComponent = function () {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
 };
 
 export default navbarComponent;
