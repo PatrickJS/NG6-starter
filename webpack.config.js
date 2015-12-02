b9dcbde90faffd59afe28f18d9a5743c033e0b9b
blackendstudios <jeroflat@gmail.com>
2015-12-02 18:06:40 -0500

    NG6 starter with babel6 working

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 
 module.exports = {
   devtool: 'sourcemap',
+   entry: [
+      // Set up an ES6-ish environment
+      'babel-polyfill',
+      // Add your application's scripts below
+      './client/app/app.js'
+   ],
   output: {
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       {
+          test: /\.js$/,
+          exclude: [/app\/lib/, /node_modules/],
+          loader: 'ng-annotate!babel'
+       },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.styl$/, loader: 'style!css!stylus' },
        { test: /\.css$/, loader: 'style!css' }
