a2753a5284b7766b8d83dfe46e8bf3236fe2a9b8
Sergey Protko <sergey.protko@intellectsoft.net>
2016-02-07 01:16:12 +0300

    chore(dependencies): Update angular to 1.5

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import angularComponent from 'angular-component';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
