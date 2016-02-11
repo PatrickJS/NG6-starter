17c6fbe900446388a75498e450a447695af724da
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-02-11 16:56:00 +0100

    added html5mode
    added .idea to gitignore
    
    fixed typo readme
    
    added history fallback for HTML5mode to work when gulp serve
    
    refactored config phase to use Inject & fat arrow
    
    removed .idea from .gitignore

--- client/app/app.js
+++ client/app/app.js
@@ -6,9 +6,15 @@ import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
-  uiRouter,
-  Common.name,
-  Components.name
-])
+    uiRouter,
+    Common.name,
+    Components.name
+  ])
+  .config(($locationProvider) => {
+    "ngInject";
+    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+    // #how-to-configure-your-server-to-work-with-html5mode
+    $locationProvider.html5Mode(true).hashPrefix('!');
+  })
 
-.component('app', AppComponent);
+  .component('app', AppComponent);
