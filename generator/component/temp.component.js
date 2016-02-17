525ecb0a7698d3d1ee08046c30d4839268160105
Gediminas Bivainis <gediminas@bivainis.com>
2016-02-17 21:06:47 +0100

    Update temp.component to use sass

--- generator/component/temp.component.js
+++ generator/component/temp.component.js
@@ -1,6 +1,6 @@
 import template from './<%= name %>.html';
 import controller from './<%= name %>.controller';
-import './<%= name %>.styl';
+import './<%= name %>.scss';
 
 let <%= name %>Component = {
   restrict: 'E',
