4f2736d10719aa2b402d86c77128b21f2c31a8f9
Gediminas Bivainis <gediminas@bivainis.com>
2016-02-17 21:05:46 +0100

    Update karma.conf and temp.component to use sass

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
         loaders: [
           { test: /\.js/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
           { test: /\.html/, loader: 'raw' },
-          { test: /\.styl$/, loader: 'style!css!stylus' },
+          { test: /\.scss$/, loader: 'style!css!sass' },
           { test: /\.css$/, loader: 'style!css' }
         ]
       }
