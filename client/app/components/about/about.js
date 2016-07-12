1e861070db71ceb6a0ceffa73353ba24e15cfaec
Samitha Fernando <fernando.samitha@gmail.com>
2016-07-12 10:11:11 +1000

    [dev] - updated spec's to use $componentController. Updated ui-router version to latest which allows routes to use components instead templates

--- client/app/components/about/about.js
+++ client/app/components/about/about.js
@@ -11,7 +11,7 @@ let aboutModule = angular.module('about', [
   $stateProvider
     .state('about', {
       url: '/about',
-      template: '<about></about>'
+      component: 'about'
     });
 })
 
