dad42f146d9cc04fd36a9c0766fb6133313f861e
Chetan Karande <chetan.yjc@gmail.com>
2016-10-19 12:16:06 -0400

    Heroku app

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var express= require('express');
+var path = require('path');
+
+var app = express();
+
+var static_path = path.join(__dirname, './../dist');
+
+app.enable('trust proxy');
+
+app.route('/').get(function(req, res) {
+    res.header('Cache-Control', "max-age=60, must-revalidate, private");
+    res.sendFile('index.html', {
+        root: static_path
+    });
+});
+
+function nocache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+}
+
+app.use('/', express.static(static_path, {
+    maxage: 31557600
+}));
+
+var server = app.listen(process.env.PORT || 5000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
