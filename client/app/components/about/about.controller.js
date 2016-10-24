000ac2aca4e13ae60821ed4eacc5abd62ee660f3
Chetan Karande <chetan.yjc@gmail.com>
2016-10-23 20:06:15 -0400

    Add appd  agent

--- client/app/components/about/about.controller.js
+++ client/app/components/about/about.controller.js
@@ -1,8 +1,37 @@
 class AboutController {
   constructor() {
     this.name = 'about';
-    //newrelic.addPageAction();
-  }
+
+    var vPageView = new ADRUM.events.VPageView({
+      url: '/#about'
+    });
+
+    vPageView.start();
+
+  // SPA view routing and HTML partials fetching
+    vPageView.markViewChangeStart();
+  // AJAX requests for the HTML partials are automatically correlated with the VPageView
+  setTimeout(3000, function (){
+    vPageView.markViewChangeEnd();
+
+  // HTML partials inserted into Browser DOM tree
+
+    vPageView.markViewDOMLoaded();
+
+  // SPA HTML AJAX data fetching
+  // Data AJAX requests are automatically correlated with the VPageView
+
+
+    vPageView.markXhrRequestsCompleted();
+
+  // call this when ending a new virtual page
+    vPageView.end();
+
+    ADRUM.report(vPageView);
+  })
+
+
+    }
 }
 
 export default AboutController;
