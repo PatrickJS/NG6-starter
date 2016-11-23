c11ffe2b958c7040bd9c29ac3e85e1e349f33d6e
Sergey Protko <fesors@gmail.com>
2016-11-23 22:51:59 +0300

    feat(sass): Replace stylus with sass

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
+import Test from './test/test';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About
+  About,
+  Test
 ])
-  
+
 .name;
 
 export default componentModule;
