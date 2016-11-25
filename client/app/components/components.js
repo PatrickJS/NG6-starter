85ddcbd5d3b45cecc2fb6bd87f797e769c35c43f
jpsc <joao@voorhoede.nl>
2016-11-25 23:35:30 +0100

    fix(tests): add dependencies for sass and fix tests

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
-import Test from './test/test';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
-  Test
+  About
 ])
 
 .name;
