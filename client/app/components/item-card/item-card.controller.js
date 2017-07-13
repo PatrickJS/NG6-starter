718806d447facc331dedb6fc40b607efbf8b94a7
Ihor Yanovchyk <igorkin.man@gmail.com>
2017-07-14 00:02:53 +0300

    added item-cart component

--- client/app/components/item-card/item-card.controller.js
+++ client/app/components/item-card/item-card.controller.js
@@ -8,6 +8,32 @@ class ItemCardController {
       'test text for <li> element2',
       'test text for <li> element3'
     ];
+    this.testData = [
+      {
+        name: 'ASIN',
+        info: 'BP1AADLL',
+      },
+      {
+        name: 'Release date',
+        info: 'September 15, 2016',
+      },
+      {
+        name: 'Best Sellers Rank',
+        info: 'BP1AADLL',
+      },
+      {
+        name: 'Pricing',
+        info: 'The strikethrough price is the List Price. Savings represents a discount off the List Price.'
+      },
+      {
+        name: 'Product Dimensions',
+        info: '16.8 x 14 x 4 inches; 8.5 pounds'
+      },
+      {
+        name: 'Another info',
+        info: 'Another info'
+      }
+    ];
     this.price = 248.97;
   }
 }
